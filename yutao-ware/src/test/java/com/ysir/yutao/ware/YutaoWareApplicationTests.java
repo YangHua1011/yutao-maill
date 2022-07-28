e7c90c4c4df682f7fbfa773df63eb0e6bfa4a79a
ysir <yanghua.1011@foxmail.com>
2022-07-28 21:57:23 +0800

    ceshi tijiao

--- yutao-ware/src/test/java/com/ysir/yutao/ware/YutaoWareApplicationTests.java
+++ yutao-ware/src/test/java/com/ysir/yutao/ware/YutaoWareApplicationTests.java
@@ -3,11 +3,16 @@ package com.ysir.yutao.ware;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.math.BigDecimal;
+
+
 class YutaoWareApplicationTests {
 
 	@Test
 	void contextLoads() {
+		BigDecimal bigDecimal = new BigDecimal(0);
+		BigDecimal bigDecimal1 = new BigDecimal(1);
+		System.out.println(bigDecimal.equals(bigDecimal1));
 	}
 
 }
